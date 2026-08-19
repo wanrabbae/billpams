5e758026b5e971843cdbed240c80b9c9212b8440
Alwan <wancode.msg@gmail.com>
2026-08-19 14:49:53 +0700

    fix: pwa sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,15 @@
-const CACHE_NAME = 'billpams-pwa-v1';
-const urlsToCache = [
-  '/',
-  '/pwa/dashboard',
-  '/logo_billpam.png',
-  '/manifest.json'
-];
+const CACHE_NAME = 'billpams-pwa-v2';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    // Network-only strategy untuk menghindari konflik dengan Laravel Livewire
+    // Browser butuh ada fetch event agar tombol Install PWA muncul
+    return;
 });
